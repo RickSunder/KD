2a1810f72ecb4006a3f9c7de14b917704037c841
RickSunder <rick@sunder.nl>
2019-10-18 16:54:27 +0200

    links work

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-@app.route('/index')
-def show_index():
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/results")
+def results():
     return render_template("results.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
